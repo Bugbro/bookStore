0d4348535e2bef7b20c5c9d8b86af54705d1e6bd
Prabhat Pal <ppal6060@gmail.com>
2026-02-11 23:48:40 +0530

    user register check

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import { connectDB } from "./src/config/connectDB.js";
+import authRouter from "./src/routes/authRoutes.js";
 
 configDotenv();
 
@@ -8,9 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-app.use("/", (req, res)=>{
-    console.log("Hello world.........");
-});
+app.use(express.json());
+
+//routes
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at ${PORT}`);
