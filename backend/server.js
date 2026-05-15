b255f02a02ff12a2daabd78f234717ffb728fe9e
Prabhat Pal <ppal6060@gmail.com>
2026-05-15 23:46:40 +0530

    Frontend url

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 
 //cors setup for admin and frontend
 const allowedOrigins = [
-  "https://bookstore-8rv1.onrender.com",
+  "https://book-store-puce-seven.vercel.app/",
   "http://localhost:5173",
   "http://localhost:5174",
 ];
